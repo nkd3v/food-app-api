1c7af334ceaaeb95b7d7e70a3d87b32a71880b78
nkd3v <nnk.prsn@gmail.com>
2023-04-23 10:05:55 +0700

    Always use Swagger

--- Program.cs
+++ Program.cs
@@ -27,12 +27,8 @@ namespace FoodAppAPI
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseRouting();
 
